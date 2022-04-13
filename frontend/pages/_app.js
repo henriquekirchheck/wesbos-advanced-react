bc6699854189f5f41a54c0febde831a9e31062d2
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-13 11:13:22 -0300

    Error: ApolloProvider doesn't work

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,23 @@
 import { Page } from '../components/Page'
 import NProgress from 'nprogress'
 import Router from 'next/router'
+// import { ApolloProvider } from '@apollo/client'
 
 import '../components/styles/nprogress.css'
+import withData from '../lib/withData'
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps /* apollo */ }) {
   return (
+    // <ApolloProvider client={apollo}>
     <Page>
       <Component {...pageProps} />
     </Page>
+    // </ApolloProvider>
   )
 }
+
+export default withData(App)
