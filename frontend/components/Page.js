c4535e425e5f3e0f2a5cdd8c9fc6c8b050f837f7
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-12 21:27:32 -0300

    Added a progress bar

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,63 @@
+import styled, { createGlobalStyle } from 'styled-components'
 import { Header } from './Header'
 
-export function Page({ children, cool }) {
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  :root {
+    --red: #ff0000;
+    --black: #393939;
+    --gray: #3a3a3a;
+    --light-gray: #e1e1e1;
+    --off-white: #ededed;
+    --max-width: 1000px;
+    --box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.9);
+  }
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'rednika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'rednika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`
+
+const InnerStyles = styled.div`
+  max-width: var(--max-width);
+`
+
+export function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <h2>Hello, I am the page component</h2>
-      <p>{cool}</p>
       {children}
     </div>
   )
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,17 @@
 import { Page } from '../components/Page'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }) {
   return (
     <Page>
-      <Component {...pageProps}/>
+      <Component {...pageProps} />
     </Page>
   )
 }
