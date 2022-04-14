5b4cddb2ab87cc3c2b62de330b843c3dc04eb6d4
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2022-04-14 11:14:50 -0300

    Added getInitialProps to the app

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -62,7 +62,6 @@ const GlobalStyle = createGlobalStyle`
 const theme = {}
 
 function App({ Component, pageProps, apollo }) {
-  console.log(apollo)
   return (
     <ApolloProvider client={apollo}>
       <GlobalStyle />
@@ -73,6 +72,16 @@ function App({ Component, pageProps, apollo }) {
       </ThemeProvider>
     </ApolloProvider>
   )
+}
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {}
+  if(Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  pageProps.query = ctx.query
+  return pageProps
 }
 
 export default withData(App)
